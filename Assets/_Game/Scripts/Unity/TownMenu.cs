18b3872731298eb9853c2857bc8ea85aeb9e0d6a
Baldie <david.1138@gmail.com>
2026-01-19 14:06:43 -0800

    feat: implement adventure button transition from Town to Arena

---
+++ Assets/_Game/Scripts/Unity/TownMenu.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace PocketSquire.Unity
+{
+    public class TownMenu : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            WireButtons();
+
+            // Default selection for gamepad/keyboard
+            var adventureBtn = GameObject.Find("Canvas/Interactables/btn_adventure")?.GetComponent<Button>();
+            if (adventureBtn != null)
+            {
+                adventureBtn.Select();
+            }
+        }
+
+        private void WireButtons()
+        {
+            var uiAudio = GameObject.Find("UIAudio");
+            var audioSource = uiAudio != null ? uiAudio.GetComponent<AudioSource>() : null;
+
+            var adventureBtn = GameObject.Find("Canvas/Interactables/btn_adventure")?.GetComponent<Button>();
+            if (adventureBtn != null)
+            {
+                adventureBtn.onClick.RemoveAllListeners();
+                adventureBtn.onClick.AddListener(() => StartCoroutine(PlaySoundThenLoad("Arena", adventureBtn.gameObject)));
+                
+                var sound = adventureBtn.GetComponent<MenuButtonSound>();
+                if (sound != null && audioSource != null) sound.source = audioSource;
+            }
+            
+            // Wire other buttons as needed later
+        }
+
+        private IEnumerator PlaySoundThenLoad(string sceneName, GameObject buttonObj)
+        {
+            if (buttonObj != null)
+            {
+                var menuButtonSound = buttonObj.GetComponent<MenuButtonSound>();
+                if (menuButtonSound != null && menuButtonSound.clickSound != null)
+                {
+                    menuButtonSound.PlayClick();
+                    yield return new WaitForSecondsRealtime(menuButtonSound.clickSound.length);
+                }
+            }
+
+            if (Application.CanStreamedLevelBeLoaded(sceneName))
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+            else
+            {
+                Debug.LogWarning($"[TownMenu] Scene '{sceneName}' could not be loaded. Please ensure it is in Build Settings.");
+            }
+        }
+    }
+}
